68a58cf157a48cd0a564064f681dc5ddb69e2966
moragachris <christiangeo94@gmail.com>
2023-09-21 16:33:55 -0600

    Reto parrafo de longitud fija

---
+++ dojo-challenges/src/ParrafoDeLongitudFija.java
@@ -0,0 +1,27 @@
+public class ParrafoDeLongitudFija {
+    public static String toFixedLengthParagraph(String input, int length){
+        StringBuilder sb = new StringBuilder();
+        String[] words = input.split(" ");
+        int currentLineLength = 0;
+
+        for (String word : words) {
+            if (currentLineLength + word.length() <= length) {
+                sb.append(word).append(" ");
+                currentLineLength += word.length() + 1;
+            } else {
+                sb.append("\n").append(word).append(" ");
+                currentLineLength = word.length() + 1;
+            }
+        }
+
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String entrada = "When you play the game of thrones, you win or you die.";
+        int longitud = 13;
+
+        String formatoDelParrafo = toFixedLengthParagraph(entrada, longitud);
+        System.out.println(formatoDelParrafo);
+    }
+}
